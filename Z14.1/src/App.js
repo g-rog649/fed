d0054c9c6ba1176905b1453ec405c330b4a3b8f8
g-rog649 <g.rogozinski.649@studms.ug.edu.pl>
2023-01-17 15:13:38 +0100

    Zadanie 14.1

---
+++ Z14.1/src/App.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function App() {
+  return (
+    <div className="center">
+      <div className="text">Hello!</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Z14.1/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const domContainer = document.getElementById("root");
+const root = createRoot(domContainer);
+root.render(<App />);
+console.log("ajsghdkasjdgkajsdgh");
